925aa49c2ec0cd84692a280cab865ec47fc88327
JacksonMcKay28 <111894468+JacksonMcKay28@users.noreply.github.com>
2022-10-13 02:28:14 +0000

    inputted all of the arrays (done)

--- .ccls-cache/@home@runner@HW7-P6-JacksonMcKay/main.cpp
+++ .ccls-cache/@home@runner@HW7-P6-JacksonMcKay/main.cpp
@@ -1,7 +1,14 @@
+//Title: HW7 Problem 6, Bronson 7.2 #1
+//Programmer: Jackson McKay
+//Course: CSCI 130-001 Dr. Mellenthin
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int voltages[10] = {89, 75, 82, 93, 78, 95, 81, 88, 77, 82};
+  double slopes[5] = {11.62, 13.98, 18.45, 12.68, 14.76};
+  double distances[100] = {6.29, 6.95, 7.25, 7.35, 7.40, 7.42};
+  double temps[64] = {78.2, 69.6, 68.5, 83.9, 55.4, 67.0, 49.8, 58.3, 62.5, 71.6};
+  char codes[15] = {'f', 'j', 'm', 'q', 't', 'w', 'z'};
 }
